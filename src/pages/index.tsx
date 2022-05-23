7ea9c6034ab3cc1c0d925ca44ccc1eb0fbf11835
Fabricio <fabricio.a.s@hotmail.com>
2022-05-23 19:16:15 -0300

    alterações na home e obteção dos dados via api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
-export default function Home() {
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+export async function getStaticProps(){
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+  const data = await res.json();
+  //add pokemons index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  });
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
+// export default function Home(props) {
+export default function Home({pokemons}) {
   return (
     <>
-      <h1>PokeNext</h1>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Poke<span>Next</span></h1>
+        <Image 
+          src="/images/pokebola.png"
+          width="50px"
+          height="50px"
+          alt="Pokebola"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+          {pokemons.map((pokemon) => (
+            <p key={pokemon.id}>{pokemon.name}</p>
+          ))}
+      </div>
     </>
   )
 }
---
+++ src/pages/sobre.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Sobre.module.css";
+
+export default function Sobre(){
+    return (
+        <div className={styles.sobre}>
+            <h1>Sobre o Projeto</h1>
+            <p>Projeto criado para apredizagem do Next JS por meio das aulas do professor Mathues Batisti.</p>
+        </div>
+    )
+}
